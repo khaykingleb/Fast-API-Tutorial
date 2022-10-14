aa3cb27563f0a5776670226b42a2ef4adcbd13f7
Gleb Khaykin <khaykingleb@gmail.com>
2022-10-14 18:25:37 +0400

    fix: :bug: complete cousres section

--- src/api/courses.py
+++ src/api/courses.py
@@ -1,6 +1,14 @@
 """Courses."""
 
+import typing as tp
+
 import fastapi
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from .utils import courses as courses_utils
+from ..db import get_db
+from ..schemas.course import Course, CourseCreate
 
 router = fastapi.APIRouter(
     prefix="/courses",
@@ -8,31 +16,35 @@ router = fastapi.APIRouter(
 )
 
 
-@router.get("/")
-async def read_courses():  # NOQA
-    return {"courses": []}
+@router.get("/", response_model=tp.List[Course])
+async def get_courses(db: Session = Depends(get_db)):  # NOQA
+    courses = courses_utils.get_courses(db)
+    return courses
 
 
-@router.post("/")
-async def create_course_api():  # NOQA
-    return {"courses": []}
+@router.post("/", response_model=Course)
+async def create_course(*, db: Session = Depends(get_db), course: CourseCreate):  # NOQA
+    return courses_utils.create_course(db, course)
 
 
-@router.get("/{id}")
-async def read_course():  # NOQA
-    return {"courses": []}
+@router.get("/{course_id}")
+async def read_course(*, db: Session = Depends(get_db), id: int):  # NOQA
+    db_course = courses_utils.get_course(db, id)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found.")
+    return db_course
 
 
-@router.patch("/{id}")
+@router.patch("/{course_id}")
 async def update_course():  # NOQA
     return {"courses": []}
 
 
-@router.delete("/{id}")
+@router.delete("/{course_id}")
 async def delete_course():  # NOQA
     return {"courses": []}
 
 
-@router.get("/{id}/sections")
+@router.get("/{course_id}/sections")
 async def read_course_sections():  # NOQA
     return {"courses": []}
---
+++ src/api/utils/courses.py
@@ -0,0 +1,27 @@
+"""Utilities for Courses section."""
+
+from sqlalchemy.orm import Session
+
+from ...db.models.course import Course
+from ...schemas.course import CourseCreate
+
+
+def get_course(db: Session, course_id: int):  # NOQA
+    return db.query(Course).filter(Course.id == course_id).first()
+
+
+def get_courses(db: Session):  # NOQA
+    return db.query(Course).all()
+
+
+def get_user_courses(db: Session, user_id: int):  # NOQA
+    courses = db.query(Course).filter(Course.user_id == user_id).all()
+    return courses
+
+
+def create_course(db: Session, course: CourseCreate):  # NOQA
+    db_course = Course(title=course.title, description=course.description, user_id=course.user_id)
+    db.add(db_course)
+    db.commit()
+    db.refresh(db_course)
+    return db_course
